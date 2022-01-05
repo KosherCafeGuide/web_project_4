7ba58a5f0dbf4819f1b452afb3a2555e6ca5b362
David Avraham <david.avraham100@gmail.com>
2022-01-05 12:29:04 +0200

    hotfix: removed absolute positioning, changed way popup form works, changed heart images into buttons and added hover effect to all buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 // Let's find the form in the DOM
-let formElement = document.querySelector(".edit-profile-form"); // Use the querySelector() method
-let profile = document.querySelector(".profile");
-let editButton = profile.querySelector(".profile__edit-btn");
-let overlay = profile.querySelector(".profile__overlay")
-    // Next is the form submit handler, though
-    // it won't submit anywhere just yet
+const formElement = document.querySelector(".edit-profile-form"); // Use the querySelector() method
+const profile = document.querySelector(".profile");
+const editButton = profile.querySelector(".profile__edit-btn");
+const popup = document.querySelector(".popup");
+const nameInput = formElement.querySelector(".edit-profile-form__name"); // Use querySelector()
+const groupInput = formElement.querySelector(".edit-profile-form__group"); // Use querySelector()
+const profileName = profile.querySelector(".profile__name");
+const profileGroup = profile.querySelector(".profile__group");
+// Next is the form submit handler, though
+// it won't submit anywhere just yet
 
 // Note that the function name starts with a verb
 // and describes exactly what the function does
@@ -12,44 +16,38 @@ function handleProfileFormSubmit(evt) {
     // This line stops the browser from 
     // submitting the form in the default way.
     evt.preventDefault();
-    // Having done so, we can define our own way of submitting the form.
-    // We'll explain it in more detail later.
 
-    // Let's find the form fields in the DOM
-    let nameInput = formElement.querySelector(".edit-profile-form__name"); // Use querySelector()
-    let groupInput = formElement.querySelector(".edit-profile-form__group"); // Use querySelector()
 
-    // Get the values of each field from the corresponding value property
     let newName = nameInput.value;
     let newGroup = groupInput.value;
-    // Select elements where the field values will be entered
-    let profileName = profile.querySelector(".profile__name");
-    let profileGroup = profile.querySelector(".profile__group");
-    // Insert new values using the textContent 
-    // property of the querySelector() method
+
     profileName.textContent = newName;
     profileGroup.textContent = newGroup;
-    formElement.style.visibility = "hidden";
-    overlay.style.visibility = "hidden";
+    popup.classList.remove("popup_opened");
 
 }
 
-// Connect the handler to the form:
-// it will watch the submit event
-formElement.addEventListener('submit', handleProfileFormSubmit);
+function handleProfileFormCancel(evt) {
+
+    evt.preventDefault();
+    popup.classList.remove("popup_opened");
+}
 
 function handleProfileFormOpen(evt) {
+
     evt.preventDefault();
-    let profileName = profile.querySelector(".profile__name");
-    let profileGroup = profile.querySelector(".profile__group");
+
     let currentName = profileName.textContent;
+
     let currentGroup = profileGroup.textContent;
-    let nameInput = formElement.querySelector(".edit-profile-form__name");
-    let groupInput = formElement.querySelector(".edit-profile-form__group");
+
     nameInput.value = currentName;
     groupInput.value = currentGroup;
-    overlay.style.visibility = "visible";
-    formElement.style.visibility = "visible";
+
+    popup.classList.add("popup_opened");
 
 }
+
+formElement.addEventListener('submit', handleProfileFormSubmit);
+formElement.addEventListener('reset', handleProfileFormCancel);
 editButton.addEventListener("click", handleProfileFormOpen);
